38324f92a941f80f0b96fcb58371a834dbbc0393
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-12 16:53:34 +0700

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <header>
+        <h1>Thanuseng Kevin Prachith</h1>
+      </header>
+      <main>
+        <h3>My three favorite topics so far are...</h3>
+        <ul>
+          <li>React</li>
+          <li>CSS Grid/Flexbox</li>
+          <li>Javascript Event Listeners</li>
+          <li>Bonus: Not Sequelize!</li>
+        </ul>
+      </main>
+      <footer>
+        <h3>Social Media</h3>
+        <ul>
+          <li>
+            <a href="https://www.linkedin.com/in/tkprachith/">LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/kevncrypting">GitHub</a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
+};
+
+export default About;
